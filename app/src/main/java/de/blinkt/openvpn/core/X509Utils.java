2663f24b6c57c0fd99a50b316520de207d9cd211
lkorth <luke@lukekorth.com>
2016-04-04 07:58:21 -0400

    Refactor X509Utils class

--- app/src/main/java/de/blinkt/openvpn/core/X509Utils.java
+++ app/src/main/java/de/blinkt/openvpn/core/X509Utils.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2012-2016 Arne Schwabe
- * Distributed under the GNU GPL v2 with additional terms. For full terms see the file doc/LICENSE.txt
- */
-
 package de.blinkt.openvpn.core;
 
 import java.io.ByteArrayInputStream;
@@ -12,32 +7,30 @@ import java.io.InputStream;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class X509Utils {
 
-	public static Certificate[] getCertificatesFromFile(String certfilename) throws FileNotFoundException, CertificateException {
-		CertificateFactory certFact = CertificateFactory.getInstance("X.509");
+	public static Certificate[] getCertificatesFromFile(String filename) throws FileNotFoundException,
+            CertificateException {
+		CertificateFactory factory = CertificateFactory.getInstance("X.509");
 
-        Vector<Certificate> certificates = new Vector<>();
-		if(VpnProfile.isEmbedded(certfilename)) {
-            int subIndex = certfilename.indexOf("-----BEGIN CERTIFICATE-----");
+        List<Certificate> certificates = new ArrayList<>();
+		if (VpnProfile.isEmbedded(filename)) {
+            int index = filename.indexOf("-----BEGIN CERTIFICATE-----");
             do {
-                // The java certifcate reader is ... kind of stupid
-                // It does NOT ignore chars before the --BEGIN ...
+                // The java certificate reader does not ignore chars before the --BEGIN
+                index = Math.max(0, index);
+                InputStream inputStream = new ByteArrayInputStream(filename.substring(index).getBytes());
+                certificates.add(factory.generateCertificate(inputStream));
 
-                subIndex = Math.max(0, subIndex);
-                InputStream inStream = new ByteArrayInputStream(certfilename.substring(subIndex).getBytes());
-                certificates.add(certFact.generateCertificate(inStream));
-
-                subIndex = certfilename.indexOf("-----BEGIN CERTIFICATE-----", subIndex+1);
-            } while (subIndex > 0);
+                index = filename.indexOf("-----BEGIN CERTIFICATE-----", index + 1);
+            } while (index > 0);
             return certificates.toArray(new Certificate[certificates.size()]);
         } else {
-			InputStream inStream = new FileInputStream(certfilename);
-            return new Certificate[] {certFact.generateCertificate(inStream)};
+			InputStream inputStream = new FileInputStream(filename);
+            return new Certificate[] {factory.generateCertificate(inputStream)};
         }
-
-
 	}
 }
