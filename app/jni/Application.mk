41464b67111d8158861d4f3405b2ef16fa77acc0
lkorth <luke@lukekorth.com>
2016-08-05 14:36:30 -0400

    Update Android ndk platform to 19

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,11 +1,3 @@
-APP_ABI := arm64-v8a armeabi armeabi-v7a mips  x86 x86_64
-APP_PLATFORM := android-14
-
-APP_STL:=stlport_static
-#APP_STL:=gnustl_shared 
-
-#APP_OPTIM := release
-
-#LOCAL_ARM_MODE := arm
-
-#NDK_TOOLCHAIN_VERSION=clang
+APP_ABI := arm64-v8a armeabi armeabi-v7a mips x86 x86_64
+APP_PLATFORM := android-19
+APP_STL := stlport_static
