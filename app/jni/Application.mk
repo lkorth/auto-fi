b61b97ffe37ad4af89ce58ed4698f3041cc65824
lkorth <luke@lukekorth.com>
2017-01-05 10:36:18 -0500

    Remove mips support

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := arm64-v8a armeabi armeabi-v7a mips x86 x86_64
+APP_ABI := arm64-v8a armeabi armeabi-v7a x86 x86_64
 APP_PLATFORM := android-19
 APP_STL := stlport_static
 APP_CPPFLAGS += -std=c++1y
